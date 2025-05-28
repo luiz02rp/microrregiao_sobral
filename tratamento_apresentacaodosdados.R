0f8dc71f285b0ec4336fe9c318f05b047898461e
Lappsie UFC <luizljsetembjose@alu.ufc.br>
2025-05-28 13:55:36 -0300

    juntar bancos de dados

--- tratamento_apresentacaodosdados.R
+++ tratamento_apresentacaodosdados.R
@@ -1,13 +1,18 @@
 # carregando pacotes ------------------------------------------------------
 library(tidyverse)
 library(janitor)
-
-
+library(sf)
+library(scales)
+library(geobr)
 # chamando banco de dados -------------------------------------------------
+# população do ceará
+ce_municipios <- read_municipality(code_muni = "CE", year = 2024)
+# população da micro sobral
 pop_micro_sobral <- read_csv2("data/micro_sobral.csv")
 
 
 # tratamento dos dados ----------------------------------------------------
+# população dos municípios da micro sobral
 pop_micro_sobral <- pop_micro_sobral |> 
   clean_names() |> 
   as_tibble() |> 
@@ -15,6 +20,11 @@ pop_micro_sobral <- pop_micro_sobral |>
     municipio = str_to_title(municipio), # garante que a primeira letra seja maiúscula
     populacao = as.numeric(populacao) # garante que os valores sejam numéricos
   )
+# juntando bancos de dados
+ce_municipios <- ce_municipios |> 
+  mutate(
+    name_muni = str_to_title(name_muni)) |> 
+  inner_join(pop_micro_sobral, by = c("name_muni" = "municipio"))
 
 # análise descritiva ------------------------------------------------------
 pop_micro_sobral |> 
@@ -29,5 +39,7 @@ pop_micro_sobral |>
   )
 
 # análise gráfica ---------------------------------------------------------
+# A) Mapa de Calor Hierárquico
+
 
 
