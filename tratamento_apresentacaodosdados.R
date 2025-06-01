8bfef90d950fb8c7f80697e76b29dd0d175d626b
unknown <luizljsetembjose@alu.ufc.br>
2025-05-31 23:22:51 -0300

    novo banco de dados relativo à mortalidade. próximo passo: fazer uma descritiva e um gráfico de mortalidade

--- tratamento_apresentacaodosdados.R
+++ tratamento_apresentacaodosdados.R
@@ -4,11 +4,17 @@ library(janitor)
 library(sf)
 library(scales)
 library(geobr)
+library(readr)
 # chamando banco de dados -------------------------------------------------
 # população do ceará
 ce_municipios <- read_municipality(code_muni = "CE", year = 2024)
+
 # população da micro sobral
 pop_micro_sobral <- read_csv2("data/micro_sobral.csv")
+
+# mortalidade da micro sobral
+mort_micro_sobral <- read_csv("data/mortalidade_micro_sobral.csv",
+                              locale = locale(encoding = "Latin1")) 
 
 
 # tratamento dos dados ----------------------------------------------------
@@ -26,6 +32,11 @@ ce_municipios <- ce_municipios |>
     name_muni = str_to_title(name_muni)) |> 
   inner_join(pop_micro_sobral, by = c("name_muni" = "municipio"))
 
+# tratando banco da mortalidade
+mort_micro_sobral <- mort_micro_sobral |> 
+  clean_names() |> 
+  rename(municipio = munici_pio)
+
 # análise descritiva ------------------------------------------------------
 pop_micro_sobral |> 
   summarise(
@@ -40,6 +51,20 @@ pop_micro_sobral |>
 
 # análise gráfica ---------------------------------------------------------
 # A) Mapa de Calor Hierárquico
+ce_municipios |> 
+  ggplot()+
+  geom_sf(aes(fill = populacao), color = "white", size = 0.1) +
+  scale_fill_viridis_c(option = "inferno", labels = comma) +
+  labs(
+    title = "Distribuição populacional da Microrregião de Sobral",
+    fill = "População"
+  ) +
+  theme_void()
 
+ce_municipios |> 
+  ggplot(
+    aes(y = name_muni)
+  )+
+  geom_col()
 
 
